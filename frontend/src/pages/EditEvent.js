8fde9ba6e84075f85339ef30034cfb72d094e2d9
Bilal <bks1733@gmail.com>
2023-05-17 15:31:15 +0530

    Reusing Actions via Request Methods.

--- frontend/src/pages/EditEvent.js
+++ frontend/src/pages/EditEvent.js
@@ -7,7 +7,7 @@ function EditEventPage() {
   const data = useRouteLoaderData('event-detail');
   return (
     <Fragment>
-      <EventForm event={data.event} />
+      <EventForm method="patch" event={data.event} />
     </Fragment>
   );
 }
--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,43 +1,12 @@
-import { json, redirect } from 'react-router-dom';
-
 import { Fragment } from 'react';
 import EventForm from '../components/EventForm';
 
 function NewEventPage() {
   return (
     <Fragment>
-      <EventForm />
+      <EventForm method="post" />
     </Fragment>
   );
 }
 
 export default NewEventPage;
-
-export async function action({ request, params }) {
-  const data = await request.formData();
-
-  const eventData = {
-    title: data.get('title'),
-    image: data.get('image'),
-    date: data.get('date'),
-    description: data.get('description'),
-  };
-
-  const response = await fetch('http://localhost:8080/events', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(eventData),
-  });
-
-  if (response.status === 422) {
-    return response;
-  }
-
-  if (!response.ok) {
-    throw json({ message: 'Could not save event.' }, { status: 500 });
-  }
-
-  return redirect('/events');
-}
